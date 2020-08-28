bc5893009940ed252fe1f06368663b64cd3fe4ad
Printo3DJob <grawires@gmail.com>
2020-08-28 17:00:41 +0200

    gyt

--- nRF24-Odbiornik/src/main.cpp
+++ nRF24-Odbiornik/src/main.cpp
@@ -6,23 +6,87 @@
 * 
 * Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
 */
+
+
+
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
 RF24 radio(9, 10); // CE, CSN
-const byte address[13] = "111000111000";
+const byte address[17] = "1100110011001100";
+float bme_data;
+int data=0;
+
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   radio.begin();
   radio.openReadingPipe(0, address);
   radio.setPALevel(RF24_PA_LOW);
   radio.startListening();
+  pinMode(10, OUTPUT);
 }
+
 void loop() {
   if (radio.available()) {
-    char text[32] = "";
-    int data=0;
-    radio.read(&data, sizeof(data));
-    Serial.println(data);
+    radio.read(&bme_data, sizeof(bme_data));
+    Serial.print("Odebrano:");
+    Serial.println(bme_data);
   }
 }
+
+/*
+
+// SimpleRx - the slave or the receiver
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+#define CE_PIN   9
+#define CSN_PIN 10
+
+const byte thisSlaveAddress[5] = {'R','x','A','A','A'};
+
+RF24 radio(CE_PIN, CSN_PIN);
+
+char dataReceived[10]; // this must match dataToSend in the TX
+bool newData = false;
+
+//===========
+
+void setup() {
+
+    Serial.begin(115200);
+
+    Serial.println("SimpleRx Starting");
+    radio.begin();
+    radio.setDataRate( RF24_250KBPS );
+    radio.openReadingPipe(1, thisSlaveAddress);
+    radio.startListening();
+}
+
+//=============
+void getData() {
+    if ( radio.available() ) {
+        radio.read( &dataReceived, sizeof(dataReceived) );
+        newData = true;
+    }
+}
+
+void showData() {
+    if (newData == true) {
+        Serial.print("Data received ");
+        Serial.println(dataReceived);
+        newData = false;
+    }
+}
+
+void loop() {
+    getData();
+    showData();
+    delay(50);
+}
+
+//==============
+
+*/
