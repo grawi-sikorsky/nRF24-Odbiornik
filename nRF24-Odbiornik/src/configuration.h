d4b35cd79d9bff9f72c0394bd7fbe9a8ba5c1b4d
JobLaptok <grawires@gmail.com>
2023-08-23 16:44:40 +0200

    config for marcin 23.08.23

--- nRF24-Odbiornik/src/configuration.h
+++ nRF24-Odbiornik/src/configuration.h
@@ -51,9 +51,9 @@
 // Define default hardcoded values using
 // [ time, blinkTime, enabled, relayNumber, relayType, evoker]
 #define DEFAULT_RELAY_1 {2000U, 250U, 1U, 1U, 1U, 0U}
-#define DEFAULT_RELAY_2 {1000U, 250U, 1U, 2U, 1U, 0U}
-#define DEFAULT_RELAY_3 {1000U, 1000U, 0U, 3U, 0U, 0U}
-#define DEFAULT_RELAY_4 {1000U, 1000U, 0U, 4U, 0U, 0U}
+#define DEFAULT_RELAY_2 {1000U, 250U, 1U, 2U, 1U, 2U}
+#define DEFAULT_RELAY_3 {1000U, 1000U, 1U, 3U, 0U, 1U}
+#define DEFAULT_RELAY_4 {1000U, 1000U, 1U, 4U, 0U, 1U}
 #define DEFAULT_RELAY_5 {800U, 300U, 0U, 5U, 0U, 0U}
 #define DEFAULT_RELAY_6 {900U, 300U, 0U, 6U, 0U, 1U}
 #define DEFAULT_RELAY_7 {1000U, 300U, 0U, 7U, 0U, 0U}
