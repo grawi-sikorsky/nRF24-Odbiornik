57ce4670155845beb2de8bf674afa2e0e824ade8
JobLaptok <grawires@gmail.com>
2023-08-28 20:44:16 +0200

    config

--- nRF24-Odbiornik/src/configuration.h
+++ nRF24-Odbiornik/src/configuration.h
@@ -49,15 +49,19 @@
 #define EEPROM_INIT_PLACE 1023
 
 // Define default hardcoded values using
+// relayType: 0 Solid, 1 Blink
+// evoker: 0 Whistle, 1 WhistleButton, 2 Helper, 3 Physical
 // [ time, blinkTime, enabled, relayNumber, relayType, evoker]
-#define DEFAULT_RELAY_1 {2000U, 250U, 1U, 1U, 1U, 0U}
-#define DEFAULT_RELAY_2 {2000U, 250U, 1U, 2U, 1U, 2U}
-#define DEFAULT_RELAY_3 {1000U, 1000U, 1U, 3U, 0U, 1U}
-#define DEFAULT_RELAY_4 {1000U, 1000U, 1U, 4U, 0U, 1U}
-#define DEFAULT_RELAY_5 {800U, 300U, 0U, 5U, 0U, 0U}
-#define DEFAULT_RELAY_6 {900U, 300U, 0U, 6U, 0U, 1U}
-#define DEFAULT_RELAY_7 {1000U, 300U, 0U, 7U, 0U, 0U}
-#define DEFAULT_RELAY_8 {1200U, 300U, 0U, 8U, 0U, 0U}
+
+//                      {time,      blink,  ena,    num,    type,   evoker}
+#define DEFAULT_RELAY_1 {2000U,		250U,	1U,		1U,		1U,		0U}
+#define DEFAULT_RELAY_2 {2000U,		250U,	1U,		2U,		1U,		2U}
+#define DEFAULT_RELAY_3 {1000U,		1000U,	1U,		3U,		0U,		0U}
+#define DEFAULT_RELAY_4 {1000U,		1000U,	1U,		4U,		0U,		1U}
+#define DEFAULT_RELAY_5 {1000U,		1000U,	1U,		5U,		0U,		2U}
+#define DEFAULT_RELAY_6 {900U,		300U,	0U,		6U,		0U,		1U}
+#define DEFAULT_RELAY_7 {1000U,		300U,	0U,		7U,		0U,		0U}
+#define DEFAULT_RELAY_8 {1200U,		300U,	0U,		8U,		0U,		0U}
 
 #ifdef DEBUGSERIAL
     #define DEFAULT_RELAYS [RELAYS_COUNT] { \
